ce2374a28bb9c03d7927278dfd00b7cf25b63c01
ShpotinDenis <55693183+ShpotinDenis@users.noreply.github.com>
2021-02-04 14:39:05 +0600

    Add files via upload

--- city/.description.php
+++ city/.description.php
@@ -2,9 +2,8 @@
 if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
 
 $arComponentDescription = array(
-	"NAME" => "Компонент цвета",
+	"NAME" => "Город по IP",
 	"DESCRIPTION" => "",
-	"SORT" => 20,
 	"CACHE_PATH" => "Y",
 	"PATH" => array(
 		"ID" => "simplecomponent",

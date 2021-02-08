68732f32c1603a32541770037dc4188c37b7aea3
ShpotinDenis <55693183+ShpotinDenis@users.noreply.github.com>
2021-02-08 19:36:16 +0600

    Add files via upload

---
+++ tw/.description.php
@@ -0,0 +1,14 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+	"NAME" => "Рабочее время",
+	"DESCRIPTION" => "",
+	"CACHE_PATH" => "Y",
+	"PATH" => array(
+		"ID" => "simplecomponent",
+        "NAME" => "Мои компоненты",
+	),
+);
+
+?>
